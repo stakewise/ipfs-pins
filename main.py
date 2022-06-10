243021dd0473893025550c41bc0e21f83560db30
cyc60 <avsysoev60@gmail.com>
2022-06-10 12:06:31 +0300

    Add raw files upload

--- main.py
+++ main.py
@@ -5,18 +5,11 @@ import ipfshttpclient
 import requests
 
 IPFS_PIN_ENDPOINT = "/dns/ipfs.infura.io/tcp/5001/https"
-IPFS_PINATA_PIN_ENDPOINT = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+IPFS_PINATA_PIN_ENDPOINT_JSON = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+IPFS_PINATA_PIN_ENDPOINT_FILE = "https://api.pinata.cloud/pinning/pinFileToIPFS"
 IPFS_PINATA_API_KEY = os.getenv("PINATA_API_KEY")
 IPFS_PINATA_SECRET_KEY = os.getenv("PINATA_SECRET_KEY")
 
-def add_ipfs_prefix(ipfs_id: str) -> str:
-    if ipfs_id.startswith("ipfs://"):
-        ipfs_id = ipfs_id[len("ipfs://") :]
-
-    if not ipfs_id.startswith("/ipfs/"):
-        ipfs_id = "/ipfs/" + ipfs_id
-
-    return 
 
 def upload_data_to_ipfs():
     """Submits files to the IPFS and pins the file."""
@@ -28,13 +21,20 @@ def upload_data_to_ipfs():
         with ipfshttpclient.connect(IPFS_PIN_ENDPOINT) as client:
             for file in files:
                 with open('data/'+file) as json_file:
-                    data = json.load(json_file)
-                    ipfs_id = client.add_json(data)
-                    client.pin.add(ipfs_id)
-                    infura_ids.append(file + ": /ipfs/" + ipfs_id)
+                    # json content
+                    try:
+                        data = json.load(json_file)
+                        ipfs_id = client.add_json(data)
+                        client.pin.add(ipfs_id)
+                        infura_ids.append(file + ": /ipfs/" + ipfs_id)
+
+                    # raw context
+                    except ValueError:
+                        ipfs_id = client.add('data/' + file)
+                        infura_ids.append(file + ": /ipfs/" + ipfs_id['Hash'])
+
     except Exception as e:
-            print(e)
-            print(f"Failed to submit to {IPFS_PIN_ENDPOINT}")
+            print(f"Failed to submit to {IPFS_PIN_ENDPOINT}. Error: {e}")
 
     if not infura_ids:
         raise Exception("Failed to submit to IPFS")
@@ -51,22 +51,37 @@ def upload_data_to_ipfs():
         try:
              for file in files:
                 with open('data/'+file) as json_file:
-                    data = json.load(json_file)
-                    response = requests.post(
-                        headers=headers,
-                        url=IPFS_PINATA_PIN_ENDPOINT,
-                        data=json.dumps({"pinataContent": data}, sort_keys=True),
-                    )
-                    response.raise_for_status()
-                    ipfs_id = response.json()["IpfsHash"]
-                    pinata_ids.append(file + ": /ipfs/" + ipfs_id)
+                    # json content
+                    try:
+                        data = json.load(json_file)
+                        response = requests.post(
+                            headers=headers,
+                            url=IPFS_PINATA_PIN_ENDPOINT_JSON,
+                            data=json.dumps({"pinataContent": data}, sort_keys=True),
+                        )
+                        response.raise_for_status()
+                        ipfs_id = response.json()["IpfsHash"]
+                        pinata_ids.append(file + ": /ipfs/" + ipfs_id)
+
+                    # raw context
+                    except ValueError:
+                        headers.pop("Content-Type")
+                        response = requests.post(
+                            headers=headers,
+                            url=IPFS_PINATA_PIN_ENDPOINT_FILE,
+                            files={'file': open('data/' + file, 'rb')}
+                        )
+                        response.raise_for_status()
+                        ipfs_id = response.json()["IpfsHash"]
+                        pinata_ids.append(file + ": /ipfs/" + ipfs_id)
+
         except Exception as e:  # noqa: E722
-            print(e)
-            print("Failed to submit to Pinata")
+            print(f"Failed to submit to Pinata. Error: {e}")
 
     if not pinata_ids:
         raise Exception("Failed to submit to IPFS")
 
     print("Pinata IDs: ", pinata_ids)
+
 
 upload_data_to_ipfs()
